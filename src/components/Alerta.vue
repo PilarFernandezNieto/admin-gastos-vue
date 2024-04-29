4a839f081479b22e96fff55a689300bf7bd1ac3c
Pilar Fernández Nieto <rla10309@educastur.es>
2024-04-29 20:00:08 +0200

    primer commit

---
+++ src/components/Alerta.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="alerta">
+
+    <slot></slot>
+
+
+    </div>
+</template>
+
+
+
+<style scoped>
+.alerta {
+    padding: 1rem;
+    font-weight: 900;
+    text-transform: uppercase;
+    font-size: 1.6rem;
+    text-align: center;
+    background-color: var(--blanco);
+    border-left: .5rem solid #b01c1c;
+    color: #b01c1c;
+    margin-bottom: 2rem;
+}
+
+</style>
